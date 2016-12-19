4ef470d9b8a7a9f9b131f511e5c152f2cdf13e8a
brittwalker <bwalk9208@gmail.com>
2016-12-19 17:18:59 -0500

    pseudocode updates

--- js/main.js
+++ js/main.js
@@ -1,15 +1,21 @@
+//Wait for doc to be ready until loading js
 $(document).ready(function () {
-
+//on the click of the submit button, do the following
 $("#submit-btn").click(function(event){
+  //don't submit the form!
   event.preventDefault();
+  //take away any extra classes from setting the attr previously
     $("body").removeClass("nyc");
     $("body").removeClass("sf");
     $("body").removeClass("la");
     $("body").removeClass("austin");
     $("body").removeClass("sydney");
+  //declare city variable to store input
     var city;
+  //match the variable value against a set of conditions
     city = $("#city-type").val();
     if (city === "NYC"||city === "New York"||city === "New York City"){
+      //what to do when there's a match - change the first class associated with "body"
     $("body").attr("class","nyc");
     $("#comingsoon").hide();
   } else if(city === "San Francisco"||city === "SF"||city === "Bay Area"){
@@ -23,6 +29,7 @@ $("#submit-btn").click(function(event){
     $("#comingsoon").hide();
   } else if(city === "Sydney"||city === "SYD"){
     $("body").attr("class","sydney");
+    //adding a little extra so ppl don't get confused when they put in a different city
     $("#comingsoon").hide();
   } else {
     $("#comingsoon").show();
